dbe2e4d5a0625df0efe73a8a72aed45d0c18f523
Oscar-Sanabria <sanabriaoscarjavier1@gmail.com>
2025-04-10 16:39:33 -0500

    first commit

---
+++ src/test/java/com/uptc/idiomas/certigest/CertigestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uptc.idiomas.certigest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertigestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
