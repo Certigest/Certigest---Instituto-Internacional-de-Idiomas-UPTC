52a500321cfd8d71d0a8c9b3a2d81e2736252ca7
LVanesa6 <laura.fernandez01@uptc.edu.co>
2025-04-20 12:33:23 -0500

    Subo frontend en React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [mensaje, setMensaje] = useState(''); // Estado para almacenar la respuesta
+
+  useEffect(() => {
+    // Realiza una solicitud GET al backend
+    axios.get('http://localhost:8080/public/hello')  // Cambia la URL si es necesario
+      .then((response) => {
+        setMensaje(response.data);  // Guarda la respuesta en el estado
+      })
+      .catch((error) => {
+        console.error('Hubo un error con la solicitud:', error);
+        setMensaje('Error al obtener mensaje');
+      });
+  }, []);  // El arreglo vacío asegura que solo se ejecute al cargar el componente
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Mensaje desde Spring Boot:</h1>
+      <p>{mensaje}</p>  {/* Muestra el mensaje obtenido del backend */}
     </div>
   );
 }
