02ac802e834cf3824c57e4d4e1dc72d1bd54c131
Diego Bayona <diegobayona36@gmail.com>
2025-05-01 18:46:31 -0500

    Configuracion para despliegue en AWS

--- src/main/java/com/uptc/idiomas/certigest/config/WebConfig.java
+++ src/main/java/com/uptc/idiomas/certigest/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000/")
+                        .allowedOrigins("https://d16uzpy2u8y3eb.cloudfront.net")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
