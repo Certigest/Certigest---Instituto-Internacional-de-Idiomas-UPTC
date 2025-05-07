9b51ea7318caf57747eb474d94e55c4336223224
Diego Bayona <diegobayona36@gmail.com>
2025-05-07 12:36:41 -0500

    Correccion

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -22,42 +22,26 @@ function Sidebar({ isOpen, onClose, onToggleSidebar }) {
 
   return (
     <>
+      {/* Botón solo visible en pantallas pequeñas */}
       <button
-        className="btn btn-warning d-md-none m-3"
+        className="btn btn-outline-primary d-md-none m-2 sidebar-toggle-btn"
         onClick={onToggleSidebar}
       >
         ☰ Menú
       </button>
-  
-      <nav
-        className={`sidebar bg-light p-3 border-end shadow-sm ${
-          isOpen ? 'd-block' : 'd-none'
-        } d-md-block`}
-        style={{ minHeight: '100vh', width: '250px' }}
-      >
-        <div className="d-flex flex-column gap-3">
-          <Link className="btn btn-outline-primary text-start" to="/cuenta" onClick={handleLinkClick}>
-            Cuenta
-          </Link>
-          <Link className="btn btn-outline-primary text-start" to="/usuarios" onClick={handleLinkClick}>
-            Usuarios
-          </Link>
-          <Link className="btn btn-outline-primary text-start" to="/cursos" onClick={handleLinkClick}>
-            Cursos
-          </Link>
-          <Link className="btn btn-outline-primary text-start" to="/certificados" onClick={handleLinkClick}>
-            Certificados
-          </Link>
-          <Link className="btn btn-outline-primary text-start" to="/inscripcion" onClick={handleLinkClick}>
-            Inscripción
-          </Link>
-          <Link className="btn btn-outline-primary text-start" to="/reportes" onClick={handleLinkClick}>
-            Reportes
-          </Link>
+
+      <nav className={`sidebar bg-light p-3 ${isOpen ? 'open' : ''}`}>
+        <div className="d-flex flex-column">
+          <Link className="sidebar-btn" to="/cuenta" onClick={handleLinkClick}>Cuenta</Link>
+          <Link className="sidebar-btn" to="/usuarios" onClick={handleLinkClick}>Usuarios</Link>
+          <Link className="sidebar-btn" to="/cursos" onClick={handleLinkClick}>Cursos</Link>
+          <Link className="sidebar-btn" to="/certificados" onClick={handleLinkClick}>Certificados</Link>
+          <Link className="sidebar-btn" to="/inscripcion" onClick={handleLinkClick}>Inscripción</Link>
+          <Link className="sidebar-btn" to="/reportes" onClick={handleLinkClick}>Reportes</Link>
         </div>
       </nav>
     </>
-  );  
+  );
 }
 
 export default Sidebar;
