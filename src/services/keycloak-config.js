b6469232b2aa6507575fe5e2534b6a1a22cdde15
LVanesa6 <laura.fernandez01@uptc.edu.co>
2025-05-09 15:57:39 -0500

    "cambio en keycloak-config"

--- src/services/keycloak-config.js
+++ src/services/keycloak-config.js
@@ -1,7 +1,7 @@
 import Keycloak from 'keycloak-js';
 
 const keycloakInstance = new Keycloak({
-  url: 'http://localhost:8081/',
+  url: process.env.REACT_APP_KEYCLOAK_URL,
   realm: process.env.REACT_APP_KEYCLOAK_REALM,
   clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID,
 });
