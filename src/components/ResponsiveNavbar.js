86811bd2e169f30c2293b484dbe7b459c386273a
Diego Bayona <diegobayona36@gmail.com>
2025-05-11 20:45:08 -0500

    Desplegar Frontend

--- src/components/ResponsiveNavbar.js
+++ src/components/ResponsiveNavbar.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import {useNavigate } from "react-router-dom";
 import { useKeycloak } from "@react-keycloak/web";
 import { Offcanvas } from "bootstrap";
 import "../styles/Header.css";
